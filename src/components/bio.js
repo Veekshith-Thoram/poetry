fbb5ab0a976e172679d805bcbda0300c4ef0929e
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2021-12-31 21:03:51 +0530

    update image

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/my-swag.jpeg"
+        src="../src/images/my-swag.jpeg"
         width={50}
         height={50}
         quality={95}
